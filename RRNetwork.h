b2c182a21a3ff256cdca6617b78809c487302e9a
Roman Resenchuk <romanres@romanres.ru>
2016-03-02 23:29:01 +0300

    Update RRNetwork.h

--- RRNetwork.h
+++ RRNetwork.h
@@ -6,7 +6,5 @@
 // Download JSON
 + (void)downloadJSONFromUrlStr:(NSString *)urlStr
 		 withCompletionHandler:(void (^)(id jsonData, NSError *error))completion;
-// Download image
-+ (void)downloadImageFromUrlStr:(NSString *)urlStr
-		  withCompletionHandler:(void (^)(UIImage *image, NSError *error))completion;
+		 
 @end
